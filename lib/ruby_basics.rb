89cdbb139fe691bc6573e228ccd866cb3628629c
magical-flow-3186 <rregalado17@gmail.com>
2019-05-27 21:21:52 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ num1 = 12
+ num2 = 2
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(name)
+  return name
 end
 
-def greeting
+def greeting(a, b)
 
 end
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
-
+"experts"
+ return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
